227e31c755fd2055101ff59243fa894d84a2825f
Kisung <kisung.dev@gmail.com>
2019-08-02 16:40:25 +0900

    scripts

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-app.use(express.static(path.join(__dirname, "../script")));
+app.use(express.static(path.join(__dirname, "../scripts")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
